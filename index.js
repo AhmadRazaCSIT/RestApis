3defbfbd2cd361e6535d72cdbce73d7c5434e97e
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-04-14 16:34:51 +0500

    Adding Routes Structure to the endpoints

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const express  = require('express');
+
+const app = express();
+
+const PORT    = process.env.PORT || 3000;
+
+//Routes 
+// Https Method GET, POST, PUT, PATCH, DELETE
+app.get('/', (req, res,next) => {
+    res.json({message:"This is New Tutorail  I have been working on that is Amazing"})
+    
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+
+})
